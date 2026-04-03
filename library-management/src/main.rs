079041514a537e6e9fd0616158805f525662f624
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-03 20:12:52 +0530

    feat(library-management): borrow method added in Book

--- library-management/src/main.rs
+++ library-management/src/main.rs
@@ -1,21 +1,49 @@
 struct Book {
     title: String,
     author: String,
-    is_available: String
+    is_available: bool,
 }
 
 struct Library {
     name: String,
     address: String,
-    book: String
+    book: Option<Book>, // Library may or may not have a book
 }
 
 enum LibraryError {
     BookNotAvailable,
     BookNotFound,
-    AlreadyBorrowed
+    AlreadyBorrowed,
+}
+
+impl Book {
+    fn borrow(&mut self) -> Result<&mut Book, LibraryError> {
+        if self.is_available == true {
+            self.is_available = false;
+            Ok(self)
+        } else {
+            Err(LibraryError::AlreadyBorrowed)
+        }
+    }
+    fn return_book() {}
+}
+
+impl Library {
+    fn add_book() {}
+    fn borrow_book(&mut self) {}
+    fn return_book() {}
 }
 
 fn main() {
-    
+    let book = Book {
+        title: String::from("The Rust book"),
+        author: String::from("Steve Klabnik"),
+        is_available: true,
+    };
+
+    let library = Library {
+        name: String::from("City library"),
+        address: String::from("123 Library Lane"),
+        book: Some(book), // assuming that there is only one book
+    };
 }

769841391cef07c52e2d93c182fa362b750c88b9
Sumeet Rana <sumeetsinh28@gmail.com>
2026-04-10 16:48:04 +0530

    feat(todo): edit_task method added

--- todo-list/src/main.rs
+++ todo-list/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
         println!("1. Add task");
         println!("2. Remove task");
         println!("3. View tasks");
-        println!("4. Exit");
+        println!("4. Edit task");
+        println!("5. Exit");
 
         io::stdin().read_line(&mut choice).expect("Invalid input");
 
@@ -19,7 +20,8 @@ fn main() {
             1 => add_task(&mut task_list),
             2 => remove_task(&mut task_list),
             3 => view_tasks(&task_list),
-            4 => {
+            4 => edit_task(&mut task_list),
+            5 => {
                 println!("Exiting...");
                 break;
             }
@@ -35,7 +37,7 @@ fn add_task(task_list: &mut Vec<String>) {
         .read_line(&mut decription)
         .expect("Invalid input");
 
-    task_list.push(decription);
+    task_list.push(decription.trim().to_string());
 }
 
 fn remove_task(task_list: &mut Vec<String>) {
@@ -62,4 +64,27 @@ fn view_tasks(task_list: &Vec<String>) {
     }
 
     println!("Task list: {:?}", task_list);
+}
+
+fn edit_task(task_list: &mut Vec<String>) {
+    if task_list.is_empty() {
+        println!("No tasks are found");
+        return;
+    }
+
+    println!("Please enter the task number that you want to edit:");
+    let mut task_number = String::new();
+    io::stdin()
+        .read_line(&mut task_number)
+        .expect("Invalid number");
+    let task_number: usize = task_number.trim().parse().expect("Invalid number");
+    println!("Please enter new task:");
+
+    let mut task_name = String::new();
+    io::stdin()
+        .read_line(&mut task_name)
+        .expect("Invalid number");
+
+    task_list.remove(task_number - 1);
+    task_list.insert(task_number - 1, task_name.trim().to_string());
 }
